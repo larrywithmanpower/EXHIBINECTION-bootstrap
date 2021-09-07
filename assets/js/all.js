aed323462bd6097080a007952d67ecb7330d192d
LokiH <larry0708@hotmail.com>
2021-09-07 15:39:49 +0800

    Update 2021-09-07T07:39:45.301Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,29 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // when window width is >= 768px
+    769: {
+      slidesPerView: 2.75,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
