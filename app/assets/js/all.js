65a750008c07b90ff5701d26fc91e7c8d00ba650
LokiH <larry0708@hotmail.com>
2021-09-07 15:39:16 +0800

    added swiper at 猜你喜歡, fixed broken layout problem at index.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,27 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 2000,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 768px
+    769: {
+      slidesPerView: 2.75,
+      spaceBetween: 30,
+    }
+  }
 });
